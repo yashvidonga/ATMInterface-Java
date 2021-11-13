d60103169995d024246ce31d22de608167cbfa2d
yashvidonga29 <yashvidonga29@gmail.com>
2021-11-13 15:13:35 +0530

    ATM Home Page Complete

---
+++ ATMInterface/src/atminterface/ATMInterface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package atminterface;
+
+import InterfaceATM.HomePage;
+
+/**
+ *
+ * @author Yashvi
+ */
+public class ATMInterface {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        HomePage form1 = new HomePage();
+        form1.setVisible(true);
+    }
+    
+}
